baa1e1cb47ec3491d4a950574602b566518fe620
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2023-04-12 22:42:55 -0300

    color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,21 @@
 module.exports = {
 	content: ['./src/**/*.{html,ts}'],
 	theme: {
+		colors: {
+			roxoflow: {
+				50: '#F9ECF0',
+				100: '#F2D4DF',
+				200: '#E6ADC2',
+				300: '#D883A2',
+				400: '#CC5C85',
+				500: '#B73968',
+				600: '#942E53',
+				700: '#6D223D',
+				800: '#4A172A',
+				900: '#230B14',
+				950: '#13060B'
+			}
+		},
 		extend: {}
 	},
 	plugins: []
