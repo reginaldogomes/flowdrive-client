e600aa616af7e7e30f2e9a1f04a87474f0422652
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2023-04-13 23:58:38 -0300

    new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './pages/index/index.component';
 import { PageErrorComponent } from './pages/page-error/page-error.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
 	{ path: '', component: IndexComponent, pathMatch: 'full' },
+	{ path: 'login', component: LoginComponent },
+	{ path: 'register', component: RegisterComponent },
 	{ path: '**', component: PageErrorComponent, pathMatch: 'full' }
 ];
 
