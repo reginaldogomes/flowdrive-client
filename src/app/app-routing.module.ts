ab5810bbca87751e75c5ba8493f607270aa52bcf
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2023-04-13 22:10:02 -0300

    page error add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './pages/index/index.component';
+import { PageErrorComponent } from './pages/page-error/page-error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', component: IndexComponent, pathMatch: 'full' },
+	{ path: '**', component: PageErrorComponent, pathMatch: 'full' }
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PageErrorComponent } from './pages/page-error/page-error.component';
 
 @NgModule({
-	declarations: [AppComponent, HeaderComponent],
+	declarations: [AppComponent, HeaderComponent, FooterComponent, PageErrorComponent],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent]
